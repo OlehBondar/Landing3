e1f7e6e1cd9d753dfda3e1560d2e728438e01bc5
Oleh Bondar <o.bondar03@ukr.net>
2024-12-22 20:31:31 +0200

    Добавив адаптації

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.header__burger');
+    const menu = document.querySelector('.header__menu');
+  
+    burger.addEventListener('click', () => {
+      menu.classList.toggle('header__menu--active');
+    });
+  });
+  
+  
